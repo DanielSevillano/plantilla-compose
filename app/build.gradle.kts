912bb48bc3b8617a8cd1e4b55a8c4e5d022228cd
Daniel <daniel-sevillano@hotmail.com>
2022-11-10 18:26:20 +0100

    Actualizando dependencias.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,13 +58,13 @@ android {
 }
 
 dependencies {
-    val composeVersion = "1.3.0"
+    val composeVersion = "1.3.1"
     val accompanistVersion = "0.27.0"
 
     implementation("androidx.activity:activity-compose:1.6.1")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.0.0")
+    implementation("androidx.compose.material3:material3:1.0.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.core:core-splashscreen:1.0.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
