08a7a60309121540c5ce388812a7209f48637470
Daniel <daniel-sevillano@hotmail.com>
2023-06-10 14:04:09 +0200

    Actualizando dependencias.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         }
     }
 
-    buildToolsVersion = "33.0.2"
+    buildToolsVersion = "34.0.0"
     namespace = "com.daniel.plantillacompose"
 }
 
@@ -61,14 +61,14 @@ dependencies {
     val composeVersion = "1.4.3"
     val accompanistVersion = "0.30.1"
 
-    implementation("androidx.activity:activity-compose:1.7.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.compose.material3:material3:1.1.0")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.navigation:navigation-compose:2.6.0")
     implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
     implementation("com.google.accompanist:accompanist-navigation-animation:$accompanistVersion")
     testImplementation("junit:junit:4.13.2")
