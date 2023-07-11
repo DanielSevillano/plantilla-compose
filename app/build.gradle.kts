b3add8ca62d44735b1c4aa9ac6ccd21d821cd656
Daniel <daniel-sevillano@hotmail.com>
2023-07-11 15:40:26 +0200

    Actualizando dependencias.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,11 @@ plugins {
 }
 
 android {
-    compileSdk = 33
-
+    compileSdk = 34
     defaultConfig {
         applicationId = "com.daniel.plantillacompose"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
