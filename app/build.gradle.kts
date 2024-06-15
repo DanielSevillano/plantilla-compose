8ff3758d147a32e256011e5b268da09902268a24
Daniel <daniel-sevillano@hotmail.com>
2024-06-15 12:41:07 +0200

    Actualizando y limpiando dependencias.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlin)
 }
 
 android {
@@ -44,10 +45,6 @@ android {
         compose = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.12"
-    }
-
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -58,21 +55,21 @@ android {
 }
 
 dependencies {
-    implementation(libs.core.ktx)
-    implementation(libs.core.splashscreen)
-    implementation(libs.lifecycle.runtime.ktx)
-    implementation(libs.activity.compose)
-    implementation(libs.navigation.compose)
     implementation(platform(libs.compose.bom))
-    implementation(libs.ui)
-    implementation(libs.ui.graphics)
-    implementation(libs.ui.tooling.preview)
-    implementation(libs.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    implementation(libs.activity.compose)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.ui)
+    implementation(libs.core.splashscreen)
+    implementation(libs.navigation.compose)
+    //implementation(libs.core.ktx)
+    //implementation(libs.lifecycle.runtime.ktx)
+    //implementation(libs.ui.graphics)
+    //implementation(libs.ui.tooling.preview)
+    //testImplementation(libs.junit)
+    //androidTestImplementation(libs.androidx.test.ext.junit)
+    //androidTestImplementation(libs.espresso.core)
     androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.ui.test.junit4)
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
+    //androidTestImplementation(libs.ui.test.junit4)
+    //debugImplementation(libs.ui.tooling)
+    //debugImplementation(libs.ui.test.manifest)
 }
